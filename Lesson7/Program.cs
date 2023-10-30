6874be81aefc870d86e91a3263b7bade8296ee70
g.l.v.9816@gmail.com <g.l.v.9816@gmail.com>
2023-10-31 00:45:30 +0300

    done

---
+++ Lesson7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lesson7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sphere firstSphere = new Sphere();
+            Console.WriteLine($"Центр окружности: {firstSphere.GetCenter()}");
+            Console.WriteLine(firstSphere.Radius);
+            firstSphere.SetCenter(3, 3, -1);
+            Console.WriteLine($"Центр окружности: {firstSphere.GetCenter()}");
+            firstSphere.Radius = 10;
+            Console.WriteLine(firstSphere.Radius);
+            Console.WriteLine($"Площадь поверхности сферы: {firstSphere.GetSquare()}");
+            Console.WriteLine($"Объём сферы: {firstSphere.GetVolume()}");
+            Console.WriteLine(firstSphere.IsPointInside(4, 5, 3));
+            Console.WriteLine(firstSphere.IsPointInside(14, 3, -1));
+
+            Console.WriteLine($"{Environment.NewLine}Вторая сфера");
+            Sphere secondSphere = new Sphere(5, 0, 1.3, 9);
+            Console.WriteLine($"Центр окружности: {secondSphere.GetCenter()}");
+            Console.WriteLine(secondSphere.Radius);
+
+            Console.WriteLine($"{Environment.NewLine}Третья сфера");
+            Sphere thirdSphere = new Sphere(2.3);
+            Console.WriteLine($"Центр окружности: {thirdSphere.GetCenter()}");
+            Console.WriteLine(thirdSphere.Radius);
+        }
+    }
+}
---
+++ Lesson7/Sphere.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Lesson7
+{
+    public class Sphere
+    {
+        private double _xCenter;
+        private double _yCenter;
+        private double _zCenter;
+        private double _radius;
+        public double Radius
+        {
+            get => _radius;
+            set => _radius = value;
+        }
+        public Sphere()
+        {
+            (_xCenter, _yCenter, _zCenter) = (0, 0, 0);
+            _radius = 1;
+        }
+        public Sphere(double radius)
+        {
+            (_xCenter, _yCenter, _zCenter) = (0, 0, 0);
+            _radius = radius;
+        }
+        public Sphere(double radius, double xCenter, double yCenter, double zCenter)
+        {
+            (_xCenter, _yCenter, _zCenter) = (xCenter, yCenter, zCenter);
+            _radius = radius;
+        }
+        public double GetVolume()
+        {
+            return 4 / 3 * Math.PI * _radius * _radius * _radius;
+        }
+        public double GetSquare()
+        {
+            return 4 * Math.PI * _radius * _radius;
+        }
+        public string GetCenter()
+        {
+            return _xCenter.ToString() + " " + _yCenter.ToString() + " " + _zCenter.ToString();
+        }
+        public void SetCenter(double x, double y, double z)
+        {
+            (_xCenter, _yCenter, _zCenter) = (x, y, z);
+        }
+        public bool IsPointInside(double x, double y, double z)
+        {
+            if ((x - _xCenter) * (x - _xCenter) + (y - _yCenter) * (y - _yCenter) + (z - _zCenter) * (z - _zCenter) < _radius * _radius)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
