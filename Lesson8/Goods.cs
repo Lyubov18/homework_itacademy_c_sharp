ed47e12e7e62da5dba3c3b74a789ec621974c453
g.l.v.9816@gmail.com <g.l.v.9816@gmail.com>
2023-11-09 23:55:08 +0300

    First

---
+++ Lesson8/Goods.cs
@@ -0,0 +1,79 @@
+﻿namespace Lesson8
+{
+    internal class Goods
+    {
+        private string _name;
+
+        private string _shopName;
+
+        private double _price;
+
+        private bool _isValid;
+        public double Price 
+        { 
+            get 
+            {
+                return _price; 
+            }            
+            set 
+            {
+                if (value > 0)
+                {
+                    _price = value;
+                }
+                else
+                {
+                    Console.WriteLine("Цена должна быть больше нуля");
+                    _isValid = false;
+                }
+            } 
+        }
+        public string ShopName
+        {
+            get
+            {
+                return _shopName;
+            }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    _shopName = value;
+                }
+                else
+                {
+                    Console.WriteLine("Название магазина не должно быть пустым");
+                    _isValid = false;
+                }
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    _name = value;
+                }
+                else
+                {
+                    Console.WriteLine("Название товара не может быть пустым");
+                    _isValid = false;
+                }
+            }
+        }
+
+        public Goods (string name, string shopName, double price)
+        {
+            Name = name;
+            ShopName = shopName;
+            Price = price;
+        }
+
+    }
+}
---
+++ Lesson8/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lesson8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Goods goods1 = new Goods("name","",-1);
+            Console.WriteLine(goods1.ShopName);
+        }
+    }
+}
