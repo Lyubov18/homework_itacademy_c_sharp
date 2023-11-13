f5fc9969a948cd71ab09f2b984b1c2d4a7bbc464
g.l.v.9816@gmail.com <g.l.v.9816@gmail.com>
2023-11-13 22:05:05 +0300

    1

---
+++ Test/MyStack.cs
@@ -0,0 +1,68 @@
+﻿namespace Test
+{
+    internal class MyStack
+    {
+        private int[] _customStack;
+
+        private int _stackSize;
+
+        private int _stackIndex;
+
+        public int StackSize 
+        {
+            get             
+            { 
+                return _stackSize;
+            }
+            private set 
+            {
+                if (value < 1)
+                {
+                    Console.WriteLine("неверно задан размер стека");
+                    _stackSize = 0;
+                }
+            }
+        }
+
+        public int[] myStack 
+        { 
+            get 
+            {
+                return _customStack; 
+            }
+        }
+
+        public MyStack(int stackSize) 
+        {
+            StackSize = stackSize;
+            _customStack = new int[_stackSize];
+            _stackIndex = 0;
+        }
+
+        public void Push(int number) 
+        {
+            if (_stackIndex > _stackSize)
+            {
+                Console.WriteLine("Стек заполнен до кокнца. Удалите последний элемент");
+            }
+            else 
+            {
+                _customStack[_stackIndex] = number;
+                _stackIndex++;
+            }
+        }
+
+        public void Pop() 
+        {
+            if (_stackIndex-1 < 0)
+            {
+                Console.WriteLine("В стеке нет значений");
+            }
+            else
+            {
+                _customStack[_stackIndex - 1] = 0;
+                _stackIndex--;
+            }
+        }
+    }
+}
