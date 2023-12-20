3697fff53464ab3be8097fb046e0dd60681866fc
g.l.v.9816@gmail.com <g.l.v.9816@gmail.com>
2023-12-20 22:10:21 +0300

    Created

--- Exam2/Order.cs
+++ Exam2/Order.cs
@@ -43,7 +43,7 @@
 
         public void PrintOrder()
         {
-            bool hasDesert = false;
+            
             foreach (var item in _dishes)
             {   
                 if (item.GetType == ) { }
