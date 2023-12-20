0f08d88172faeecb075a9c2eb22d84e3b1cc973a
g.l.v.9816@gmail.com <g.l.v.9816@gmail.com>
2023-12-20 21:43:01 +0300

    done

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+namespace Exam
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = new List<int> { 5, 3, 8, 1, 7, 2, 6, 4 };
+
+            // Сортируем коллекцию по возрастанию
+            var sortedNumbers = numbers.OrderBy(n => n).ToList();
+
+            int startIndex = numbers.Count % 2 == 0 ? numbers.Count / 2 : numbers.Count / 2 + 1;
+
+            var secondHalfSquaredDescending = sortedNumbers.Skip(startIndex).OrderByDescending(n => n * n).ToList();
+
+            // Выводим результат
+            Console.WriteLine("Исходная коллекция: " + string.Join(", ", numbers));
+            Console.WriteLine("Отсортированная по возрастанию: " + string.Join(", ", sortedNumbers));
+            Console.WriteLine("Квадраты элементов второй половины (отсортированные по убыванию): " + string.Join(", ", secondHalfSquaredDescending));
+        }
+    }
+}
